897b5655852a894bd8eb555421eaf7b53b93808c
Gabriel Jacobsohn <cg.jacobsohn@googlemail.com>
2019-07-12 16:44:54 +0200

    initial Spring Boot setup

---
+++ src/main/java/com/jacobsohn/ohlala/OhlalaApplication.java
@@ -0,0 +1,13 @@
+package com.jacobsohn.ohlala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhlalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhlalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobsohn/ohlala/OhlalaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jacobsohn.ohlala;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OhlalaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
